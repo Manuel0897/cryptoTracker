07ca8b8291a87f4645faa9c913c9bd403d4239e8
Manuel Mendoza <manuel_mm97@hotmail.com>
2021-07-05 14:03:05 -0500

    add first screen (CoinsList) & get data from API

---
+++ src/components/coins/CoinsScreen.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { View, Text, Pressable, StyleSheet, FlatList, ActivityIndicator } from 'react-native';
+import Http from '../../libs/http';
+import colors from '../../res/colors';
+import CoinsItem from './CoinsItem';
+
+
+export default class CoinsScreen extends Component {
+
+    state = {
+        coins: [],
+        allCoins: [],
+        loading: false
+    }
+    
+    componentDidMount = () => {
+        this.getCoins();
+    }
+    
+    getCoins = async () => {
+        this.setState({ loading: true });
+    
+        const res = await Http.instance.get("https://api.coinlore.net/api/tickers/");
+    
+        this.setState({ coins: res.data, allCoins: res.data, loading: false });
+    }
+
+
+    render() {
+        const { coins, loading } = this.state;
+
+        return (
+            <View style={ styles.container }>
+                { loading ?
+                    <ActivityIndicator
+                        style={styles.loader}
+                        color="#fff"
+                        size="large"
+
+                    />
+                    : null
+                }
+                <FlatList
+                    data={coins}
+                    renderItem={({item}) => (
+                        <View>
+                            <Text>{item.name}</Text>
+                        </View>
+                    )}
+                />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: colors.charade
+    },
+    titleText: {
+        color: "#222",
+        textAlign: "center"
+    },
+    loader: {
+      marginTop: 60
+    }
+})
